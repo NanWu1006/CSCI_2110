d5931afad082a3242b0585b25e9afecaa7858ea2
Kanzaki Mirai <me@idickyt.com>
2016-01-15 17:05:25 -0400

    fixed bug

--- Lab_1/Q4/Q4/QuestionFour.java
+++ Lab_1/Q4/Q4/QuestionFour.java
@@ -2,10 +2,10 @@ package Q4;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.*;
+import java.util.regex.Pattern;
 
 /**
  * Created by KanzakiMirai on 1/12/16.
@@ -26,7 +26,18 @@ public class QuestionFour {
         File file = new File(filePath);
         try {
             fileScanner = new Scanner(file);
-            startProcess(fileScanner);
+            String fileContent = fileScanner.useDelimiter("\\Z").next();
+            char[] charsArray = fileContent.toCharArray();
+
+            ArrayList<String> charsArrayList = new ArrayList<>();
+            for (char eachChar : charsArray) {
+                String eachCharString = Character.toString(eachChar).toUpperCase();
+                if (eachCharString.toCharArray()[0] >= 65 && eachCharString.toCharArray()[0] <= 90) {
+                    charsArrayList.add(Character.toString(eachChar).toUpperCase());
+                }
+            }
+
+            startProcess(charsArrayList);
         }
         catch (Exception e) {
             System.out.println("FILE OPEN FAIL, REASON: " + e.getLocalizedMessage());
@@ -35,21 +46,20 @@ public class QuestionFour {
 
     /**
      * generating the bar chart of word frequency of the given file
-     * @param fileScanner the file reader
+     * @param charsArrayList arraylist contains every char
      */
-    static void startProcess(Scanner fileScanner) {
+    static void startProcess(ArrayList<String> charsArrayList) throws UnsupportedEncodingException {
         HashMap<String, Integer> freqMap = new HashMap<>();
 
         /**
          * building a hashmap to count every word
          */
-        while (fileScanner.hasNext()) {
-            String nextWord = fileScanner.next();
-            if (freqMap.get(nextWord) == null) {
-                freqMap.put(nextWord, 1);
+        for (String eachChar : charsArrayList) {
+            if (freqMap.get(eachChar) == null) {
+                freqMap.put(eachChar, 1);
             }
             else {
-                freqMap.put(nextWord, freqMap.get(nextWord) + 1);
+                freqMap.put(eachChar, freqMap.get(eachChar) + 1);
             }
         }
 
@@ -63,14 +73,16 @@ public class QuestionFour {
             String key = entry.getKey();
             Integer value = entry.getValue();
 
-            keysString+= String.format("%s|", key);
+            keysString+= String.format("%s(%s)|", key, value);
             valuesString+= String.format("%s,", value);
         }
 
-        keysString = keysString.substring(0, keysString.length() - 1);
+        keysString = URLEncoder.encode(keysString.substring(0, keysString.length() - 1), "UTF-8");
         valuesString = valuesString.substring(0, valuesString.length() - 1);
 
         String chartImageURL = String.format("https://chart.googleapis.com/chart?&chbh=30,05,10&chxt=y&chs=1000x300&chd=t:%s&cht=bvs&chco=4D89F9&chl=%s", valuesString, keysString);
+
+        System.out.println("freqMap = " + freqMap);
 
         System.out.println("Bar chart URL: " + chartImageURL);
     }
